44628e54c28d7e46d179c80dd71fe5a11f632220
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2024-02-19 23:40:11 +0100

    add telescope keybindings

--- init.lua
+++ init.lua
@@ -465,6 +465,9 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>sc', require('telescope.builtin').commands, { desc = '[S]earch [C]ommands' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
+vim.keymap.set('n', '<leader>sp', require('telescope.builtin').spell_suggest, { desc = '[S]earch new [P]hrase' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
