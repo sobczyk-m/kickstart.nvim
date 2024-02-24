8387a3af1cabe1930dff115a7b48826d011668c9
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2024-02-24 20:06:40 +0100

    add keymapping to open toggleterm_managaer

--- lua/custom/plugins/toggleterm-manager.lua
+++ lua/custom/plugins/toggleterm-manager.lua
@@ -5,5 +5,9 @@ return {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim", -- only needed because it's a dependency of telescope
   },
-  config = true,
+  config = function()
+    vim.keymap.set('n', '<C-m>', '<cmd>Telescope toggleterm_manager<CR>', { desc = 'Toggleterm manager' })
+    require("toggleterm-manager").setup {
+    }
+  end,
 }
