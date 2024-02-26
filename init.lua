b9fbdb71a9ad9179bd7786b8594749303482dcfb
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2024-02-26 19:13:36 +0100

    add *templ commeting and format on save support

--- init.lua
+++ init.lua
@@ -311,6 +311,30 @@ require('lazy').setup({
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
+-- Formatter for *.templ files
+local templ_format = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+  vim.fn.jobstart(cmd, {
+    on_exit = function()
+      -- Reload the buffer only if it's still the current buffer
+      if vim.api.nvim_get_current_buf() == bufnr then
+        vim.cmd('e!')
+      end
+    end,
+  })
+end
+
+-- Format on save for *.templ files
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*.templ" }, callback = templ_format })
+
+-- numToStr/Comment.nvim configuration
+local ft = require('Comment.ft')
+-- Add *.templ filetype line and block commenting
+ft.set('templ', { '//%s', '//%s' })
+
 -- Set highlight on search
 vim.o.hlsearch = true
 
